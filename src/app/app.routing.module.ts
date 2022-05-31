901974c61a456ec24f14982ea3061a5c9d839d90
Ilnaz Valiev <ilvaliev@microsoft.com>
2022-06-01 02:07:06 +0300

    nav skip url changes

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -73,7 +73,10 @@ export const routes: Routes = [
 @NgModule({
     imports: [
         RouterModule.forRoot(
-            routes
+            routes,
+            {
+              initialNavigation: 'disabled'
+            }
         )
     ],
     exports: [
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
-const webpack = require('webpack');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const webpack = require("webpack");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   output: {
-    uniqueName: 'ngsandbox',
+    uniqueName: "ngsandbox",
     publicPath: "auto",
-    scriptType:'text/javascript'
+    scriptType: "text/javascript",
   },
   optimization: {
     runtimeChunk: false,
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'ngsandbox',
-      // library: { type: 'var', name: 'ngsandbox' },
-      filename: 'remoteEntry.js',
+      name: "ngsandbox",
+      filename: "remoteEntry.js",
       exposes: {
-        './ngsandbox': './src/loadApp.ts'
+        "./ngsandbox": "./src/loadApp.ts",
       },
       shared: {
-        '@angular/core': { eager: true, singleton: true },
-        '@angular/common': { eager: true, singleton: true },
-        '@angular/router': { eager: true, singleton: true },
+        "@angular/core": { eager: true, singleton: true },
+        "@angular/common": { eager: true, singleton: true },
+        "@angular/router": { eager: true, singleton: true },
       },
     }),
   ],
