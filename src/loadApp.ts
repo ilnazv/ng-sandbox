91373e45b33d93d66b24690f0ee989e279a45df8
Ilnaz Valiev <ilvaliev@microsoft.com>
2022-06-01 01:43:07 +0300

    loadApp upd

--- src/loadApp.ts
+++ src/loadApp.ts
@@ -1,17 +1,21 @@
 import "zone.js";
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 
 if (environment.production) {
   enableProdMode();
 }
 
-const mount = ()=>{
-  platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.error(err));
-}
+const mount = (container: HTMLElement) => {
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
 
-export{mount}
+  const appRoot = document.createElement('app-root');
+  container.appendChild(appRoot);
+};
+
+export { mount };
